e0096979efc2fd1a4dd71910d6d04cc576badaf5
Alex Bamford <Occidio001@gmail.com>
2023-07-19 21:21:31 +0100

    use dict to add order to dynamo

--- lambda_function.py
+++ lambda_function.py
@@ -3,33 +3,21 @@ import uuid
 from customer import Customer
 from order import Order
 from datetime import datetime
-from dynamodb_json import json_util as json 
 
 def lambda_handler(event, context):
-    # this will create dynamodb resource object and
-    # here dynamodb is resource name
-    client = boto3.resource('dynamodb')
-
-    # this will search for dynamoDB table
-    # your table name may be different
-    table = client.Table("customer")
-    print(table.table_status)
-
     rawPayload = event['detail']['payload']
 
     cust = Customer(str(uuid.uuid4()), rawPayload['customer']['id'])
-
     order = Order(rawPayload['id'], rawPayload['order_number'], rawPayload['total_price'])
 
-    dynamodb_json = json.dumps(order) 
-    print(dynamodb_json)
+    client = boto3.resource('dynamodb')
+    table = client.Table("customer")
 
     response = table.put_item(Item={
             'id': str(uuid.uuid4()),
             'customerId':cust.customerId,
             'shopifyCustomerId':cust.shopifyCustomerId,
             'upsertedAt': str(datetime.now()),
-            'orders': [order]
+            'orders':[order.asdict()]
         })
-    print(response)
     return response
--- order.py
+++ order.py
@@ -9,3 +9,6 @@ class Order:
   def toJSON(self):
         return json.dumps(self, default=lambda o: o.__dict__, 
             sort_keys=True, indent=4)
+  
+  def asdict(self):
+      return {'id': self.id, 'orderNumber': self.orderNumber, 'price': self.price}
