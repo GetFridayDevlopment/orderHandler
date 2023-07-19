a86772eadb5d413bfcb4ca5fa77b6b649f68f93d
Alex Bamford <Occidio001@gmail.com>
2023-07-19 16:09:32 +0100

    Add requirements.txt

--- lambda_function.py
+++ lambda_function.py
@@ -3,7 +3,7 @@ import uuid
 from customer import Customer
 from order import Order
 from datetime import datetime
-import json
+from dynamodb_json import json_util as json 
 
 def lambda_handler(event, context):
     # this will create dynamodb resource object and
@@ -21,13 +21,15 @@ def lambda_handler(event, context):
 
     order = Order(rawPayload['id'], rawPayload['order_number'], rawPayload['total_price'])
 
-    print(event)
+    dynamodb_json = json.dumps(order) 
+    print(dynamodb_json)
 
     response = table.put_item(Item={
             'id': str(uuid.uuid4()),
             'customerId':cust.customerId,
             'shopifyCustomerId':cust.shopifyCustomerId,
-            'upsertedAt': str(datetime.now())
+            'upsertedAt': str(datetime.now()),
+            'orders': [order]
         })
     print(response)
     return response
