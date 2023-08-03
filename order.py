914291bcdadacd83fe4597e8eba8c05518820648
Alex Bamford <Occidio001@gmail.com>
2023-08-03 20:09:54 +0100

    Update order init

--- order.py
+++ order.py
@@ -1,15 +1,16 @@
 import uuid
 
 class Order:
-  def __init__(self, shopifyId, number, price):
+  def __init__(self, sourceOrderId, souceName, number, price):
     self.id =  str(uuid.uuid4())
-    self.shopifyOrderId = shopifyId
-    self.orderNumber = number
+    self.souceName = souceName
+    self.sourceOrderId = sourceId
+    self.sourceOrderNumber = number
     self.price = price
-    self.lineItems = []
+    self.orderItems = []
 
-  def addLineItem(self, lineItem):
-    self.lineItems.append(lineItem.asdict())
+  def addOrderItem(self, lineItem):
+    self.orderItems.append(lineItem.asdict())
 
   def asdict(self):
       return {'id': self.id, 'orderNumber': self.orderNumber, 'price': self.price, 'lineItems': self.lineItems}
