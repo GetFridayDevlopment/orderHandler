2acd47e453435c88ccb8634917c2064a67cdecd2
Alex Bamford <Occidio001@gmail.com>
2023-08-03 20:05:54 +0100

    Use SourceOrder details

--- lambda_function.py
+++ lambda_function.py
@@ -9,26 +9,25 @@ def lambda_handler(event, context):
     rawPayload = event['detail']['payload']
 
     cust = Customer(str(uuid.uuid4()), rawPayload['customer']['id'])
-    order = Order(rawPayload['id'], rawPayload['order_number'], rawPayload['total_price'])
+    order = Order(rawPayload['id'], 'shopify', rawPayload['order_number'], rawPayload['total_price'])
     
     rawItems = rawPayload['line_items']
     for item in rawItems:
-        order.addLineItem(LineItem(item['sku'], item['price'])) 
+        order.addOrderItem(LineItem(item['sku'], item['price'])) 
 
     client = boto3.resource('dynamodb')
 
     orderTable = client.Table("order")
     orderResponse = orderTable.put_item(Item={
-                'id': str(uuid.uuid4()),
                 'orderId':order.id,
-                'shopifyOrderId': order.shopifyOrderId,
-                'lineItems': order.lineItems.asdict(),
+                'sourceName':order.souceName,
+                'sourceOrderId': order.sourceOrderId,
+                'orderItems': order.orderItems,
                 'upsertedAt': str(datetime.now())
             })
 
     custTable = client.Table("customer")
     custResponse = custTable.put_item(Item={
-            'id': str(uuid.uuid4()),
             'customerId':cust.customerId,
             'shopifyCustomerId':cust.shopifyCustomerId,
             'orders':[order.id],
