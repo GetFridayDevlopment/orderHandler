1b8ceb10a8b187bb560657f50d71830bdf8b956d
Mike Hughes <mike.p.hughes@hotmail.co.uk>
2023-09-27 11:23:46 +0100

    attempt at fixing requests lib

--- esim_go_client.py
+++ esim_go_client.py
@@ -1,7 +1,8 @@
-import requests
+import urllib3
 import json
 from order import Order
 import os
+
 
 class EsimGoClient:
     def __init__(self) -> None:
@@ -9,16 +10,19 @@ class EsimGoClient:
         pass
 
     def new_order(self, order: Order):
+        http = urllib3.PoolManager()
         url = 'https://api.esim-go.com/v2.2/orders'
         payload = {
             "type": "validate",
             "assign": "true",
             "Order": [{
                 "type": "bundle",
-                "quantity":1,
-                "item": order.order_items[0].sku #Temporary
+                "quantity": 1,
+                "item": order.order_items[0].sku  # Temporary
             }]
         }
         headers = {"X-API-Key": self.auth_key}
-        r = requests.post(url, data=json.dumps(payload), headers=headers)
+
+        r = http.request('POST', url, body=json.dumps(
+            payload), headers=headers)
         print(r.content)
