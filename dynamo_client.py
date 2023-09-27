93aace3ffe326fc312506b884328a5fc1ab59996
Mike Hughes <mike.p.hughes@hotmail.co.uk>
2023-09-27 12:02:37 +0100

    corrected index name

--- dynamo_client.py
+++ dynamo_client.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from boto3.dynamodb.conditions import Key
 import boto3
 
+
 class DynamoClient:
     def __init__(self):
         client = boto3.resource('dynamodb')
@@ -11,32 +12,32 @@ class DynamoClient:
     def get_customers(self, source_customer_id):
         print("source_customer_id " + str(source_customer_id))
         response = self.cust_table.query(
-                IndexName='sourceCustomerId-index',
-                KeyConditionExpression=Key('sourceCustomerId').eq(source_customer_id)
-            )
+            IndexName='source_customer_id-index',
+            KeyConditionExpression=Key(
+                'sourceCustomerId').eq(source_customer_id)
+        )
         return response['Items']
-        
+
     def put_customer(self, customer):
         response = self.cust_table.put_item(Item={
-            'customerId':customer.customer_id,
-            'sourceName':customer.souce_name,
-            'sourceCustomerId':customer.source_customer_id,
-            'orders':customer.orders,
+            'customerId': customer.customer_id,
+            'sourceName': customer.souce_name,
+            'sourceCustomerId': customer.source_customer_id,
+            'orders': customer.orders,
             'upsertedAt': customer.upserted_at
         })
 
         return response['ResponseMetadata']['HTTPStatusCode'] == 200
-    
+
     def put_order(self, order, customer):
         response = self.order_table.put_item(Item={
-                    'orderId':order.id,
-                    'sourceName':order.souce_name,
-                    'sourceOrderId': order.source_order_id,
-                    'customerId': customer.customerId,
-                    'totalPrice': order.price,
-                    'orderItems': order.order_items,
-                    'upsertedAt': str(datetime.now())
-                })
-        
-        return response['ResponseMetadata']['HTTPStatusCode'] == 200
+            'orderId': order.id,
+            'sourceName': order.souce_name,
+            'sourceOrderId': order.source_order_id,
+            'customerId': customer.customerId,
+            'totalPrice': order.price,
+            'orderItems': order.order_items,
+            'upsertedAt': str(datetime.now())
+        })
 
+        return response['ResponseMetadata']['HTTPStatusCode'] == 200
