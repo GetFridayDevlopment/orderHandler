2f3a4a04145ec76916e73490c7008ed788a06352
GetFridayDevlopment <144233875+GetFridayDevlopment@users.noreply.github.com>
2023-10-11 13:32:07 +0530

    Used sendgrid api instead fo python package (#7)
    Co-authored-by: sivasankar-selvaraj <sivasankar.selva@gmail.com>

--- send_email.py
+++ send_email.py
@@ -1,56 +1,77 @@
 import os
-from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail, Attachment, FileContent, FileName, FileType, Disposition
-import base64
+import json
+import urllib3
 from botocore.exceptions import ParamValidationError
 import boto3
 
 class EmailClient:
     def __init__(self):
-        # self.auth_key = os.environ['ESIM_GO_AUTH_KEY']
         self.api_key = os.environ['SEND_GRID_API_KEY']
-        # self.s3_client = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
         self.s3_client = boto3.client('s3')
         self.s3_bucket_name = 'esim-qrcode'
     
     def send_email_with_qr_code(self, email_to, subject, qr_code_binary, esim_details):
         try:
-            sg = SendGridAPIClient(self.api_key)
-
             # Initialize an HTML email body
-           
             print(esim_details)
+
+            # Create an instance of urllib3 PoolManager
+            http = urllib3.PoolManager()
+
             # Iterate through the list of image data dictionaries
             for image_data in qr_code_binary:
-                email_body = "<html><body>"
                 image_name = image_data['image_name']
                 image_url = image_data['image_url']
+
                 try:
-                   for esim in esim_details:
-                       if(os.path.splitext(image_name)[0] == esim['iccid']):
-                           bundle = esim['bundle']
-                   qr_code_url = 'https://esim-qrcode.s3.eu-west-2.amazonaws.com/'+image_name
-                  
-                   with open('qr_code_email_template.html', 'r') as template_file:
-                    email_template = template_file.read()   
-                   
+                    for esim in esim_details:
+                        if os.path.splitext(image_name)[0] == esim['iccid']:
+                            bundle = esim['bundle']
+                    qr_code_url = 'https://esim-qrcode.s3.eu-west-2.amazonaws.com/' + image_name
+
+                    with open('qr_code_email_template.html', 'r') as template_file:
+                        email_template = template_file.read()
+
                     # Format the template with the dynamic URL
-                    
                     email_template = email_template.replace('{{bundle}}', bundle)
                     email_template = email_template.replace('{{qr_code_url}}', qr_code_url)
 
-                  
-                    # Create a SendGrid email message
-                   message = Mail(
-                        from_email='hello@easyesim.co',
-                        to_emails=email_to,
-                        subject=subject,
-                        html_content=email_template,
+                    # Create the email data for SendGrid API
+                    email_data = {
+                        "personalizations": [
+                            {
+                                "to": [{"email": email_to}],
+                                "subject": subject
+                            }
+                        ],
+                        "from": {"email": "hello@easyesim.co"},
+                        "content": [
+                            {
+                                "type": "text/html",
+                                "value": email_template
+                            }
+                        ]
+                    }
+
+                    # Serialize email_data to JSON
+                    encoded_data = json.dumps(email_data).encode('utf-8')
+
+                    # Send the email using the SendGrid API via urllib3
+                    headers = {
+                        "Authorization": f"Bearer {self.api_key}",
+                        "Content-Type": "application/json"
+                    }
+                    response = http.request(
+                        'POST',
+                        'https://api.sendgrid.com/v3/mail/send',
+                        body=encoded_data,
+                        headers=headers
                     )
 
-                    # Send the email
-                   response = sg.send(message)
-                #    print(f"Email sent with status code: {response.status_code}")
+                    if response.status == 202:
+                        print(f"Email sent successfully with status code: {response.status}")
+                    else:
+                        print(f"Email sending failed with status code: {response.status}")
 
                 except ParamValidationError as e:
                     print(f"Skipping invalid image data: {str(e)}")
